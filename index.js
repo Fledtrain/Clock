6f77b73e891332807cedc8e0095999247702772f
Fledtrain <Joeyew2014@gmail.com>
2023-03-25 19:17:12 -0400

    Added JS for tracking time

---
+++ index.js
@@ -0,0 +1,8 @@
+setInterval(setClock, 1000);
+
+const setClock = () => {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+};
